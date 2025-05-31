e267c889edb3cff14c2565eaae71ccaec99e30e9
venkideshVenu <venkidesh.venu.04@gmail.com>
2025-05-31 16:13:13 +0530

    frontend setup

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
