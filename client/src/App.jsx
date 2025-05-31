1587c612768c257cfcdfd57c58512ef26ca96b8e
venkideshVenu <venkidesh.venu.04@gmail.com>
2025-05-31 19:03:31 +0530

    done homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
