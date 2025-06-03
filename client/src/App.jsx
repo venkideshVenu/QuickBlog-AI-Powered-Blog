77ca5c0fffa351101a53368cc60c2a53c1785f91
venkideshVenu <venkidesh.venu.04@gmail.com>
2025-06-03 21:57:58 +0530

    login and dashboard layout complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 const App = () => {
   return (
@@ -9,6 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={ true ? <Layout /> : <Login /> }>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ client/src/components/admin/Login.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full py-6 text-center">
+            <h1 className="text-3xl font-bold">
+              <span className="text-primary">Admin</span> Login
+            </h1>
+            <p className="font-light">
+              Enter your credentials to access the admin panel
+            </p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+              <label>Email</label>
+              <input
+               onChange={ e => setEmail(e.target.value) } value={email}
+                type="text"
+                required
+                placeholder="Your Email ID"
+                className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label>Password</label>
+              <input
+              onChange={ e => setPassword(e.target.value) } value={password}
+                type="password"
+                required
+                placeholder="Your Password"
+                className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
